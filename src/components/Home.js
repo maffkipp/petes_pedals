92f8559c0b70cf54313db5726dd53edf66124f9e
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2017-11-28 15:46:52 -0600

    added home page content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import '../css/Home.css';
 
+import HomeDisplay from './HomeDisplay.js';
+
 class Home extends Component {
   render() {
     return(
       <div className='home'>
-        <h1>Welcome!</h1>
+        <h1>Home</h1>
+        <div className='home-container'>
+          <HomeDisplay url='repair' color='red' name='Repairs & Restorations' data={this.props.data} type='repairsAndRestorations'/>
+          <HomeDisplay url='builds' color='white' name='Custom Pedals' data={this.props.data} type='customPedals'/>
+        </div>
+        <div className='home-container'>
+          <HomeDisplay url='mods' color='white' name='Modded Pedals' data={this.props.data} type='moddedPedals'/>
+          <HomeDisplay url='news' color='red' name='News' data={this.props.data} type='news'/>
+        </div>
       </div>
     )
   }
---
+++ src/components/HomeDisplay.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import '../css/HomeDisplay.css';
+import { Link } from 'react-router-dom';
+
+class HomeDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.name,
+      image: '',
+      subtitle: '',
+      id: ''
+    }
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData(this.props.type);
+  }
+
+  getData(type) {
+    let firstData = this.props.data.find(result => {
+       return result.sys.contentType.sys.id === type;
+    });
+    if (firstData) {
+      this.setState({
+        image: firstData.fields.headPhoto.fields.file.url,
+        subtitle: firstData.fields.title,
+        id: firstData.sys.id
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Link to={`/${this.props.url}/${this.state.id}`} className='home-display-link'>
+      <div className={`home-display ${this.props.color}`}>
+        <h2 className='home-display-title'>{this.state.title}</h2>
+        <img className='home-display-image' src={this.state.image} alt={this.state.title} />
+        <h3 className='home-display-subtitle'>{this.state.subtitle}</h3>
+      </div>
+      </Link>
+    )
+  }
+}
+
+export default HomeDisplay;
